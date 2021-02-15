84b0af99a90b67472e42a773910c06725e53480d
MasakiMiura <203426017@ccmailg.meijo-u.ac.jp>
2021-02-15 18:42:17 +0900

    SelectionData

--- front/visualize-app/src/component/SelectionData.js
+++ front/visualize-app/src/component/SelectionData.js
@@ -6,8 +6,43 @@ export default class SelectionData extends Component {
     super(props);
   }
   render() {
-    return <div className={styles.SelectionData}>
-      メタタグ絞り込み
-    </div>;
+    return (
+      <div className={styles.SelectionData}>
+        メタタグ絞り込み
+        <select>
+          <option selected value>
+            Whether
+          </option>
+          <option value="rainy">rainy</option>
+          <option value="snowy">snowy</option>
+          <option value="clear">clear</option>
+          <option value="overcast">overcast</option>
+          <option value="undefined">undefined</option>
+          <option value="party cloudy">party cloudy</option>
+          <option value="foggy">foggy</option>
+        </select>
+        <select>
+          <option selected value>
+            Scene
+          </option>
+          <option value="tunnel">tunnel</option>
+          <option value="residential<">residential</option>
+          <option value="parking lot">parking lot</option>
+          <option value="undefined">undefined</option>
+          <option value="city street">city street</option>
+          <option value="gas stations">gas stations</option>
+          <option value="highway">highway</option>
+        </select>
+        <select>
+          <option selected value>
+            Timeofday
+          </option>
+          <option value="daytime">daytime</option>
+          <option value="night">night</option>
+          <option value="dawn/dusk">dawn/dusk</option>
+          <option value="undefined">undefined</option>
+        </select>
+      </div>
+    );
   }
 }
