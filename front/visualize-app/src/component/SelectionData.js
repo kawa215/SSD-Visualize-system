641cce6af66734cef4253881f986c0255ddb4ff6
MasakiMiura <203426017@ccmailg.meijo-u.ac.jp>
2021-02-15 13:50:49 +0900

    selectionData

--- front/visualize-app/src/component/SelectionData.js
+++ front/visualize-app/src/component/SelectionData.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import styles from './SelectionData.module.css';
+import styles from "./SelectionData.module.css";
 
 export default class SelectionData extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <div>aaa</div>;
+    return <div className={styles.SelectionData}>
+      メタタグ絞り込み
+    </div>;
   }
 }
